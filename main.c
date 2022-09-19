e4332b66d1abb90a9110d8e9a7088a663e9acce4
unknown <zaaferkenza08@gmail.com>
2022-09-19 11:42:04 +0100

    done

--- main.c
+++ main.c
@@ -12,6 +12,8 @@ void main() {
 	int N=0;
 	char code[10];
 	int quant;
+	int Total;
+	float Moyenne;
 	do {
 		printf("\n\t---Menu principale---");
 		printf("\nPour inserer un nouveaux produit Entrer 1 : ");
@@ -22,7 +24,7 @@ void main() {
 		printf("\nPour afficher l'etat de stock Entrer 6 : ");
 		printf("\nPour alimenter le stock Entrer 7 : ");
 		printf("\nPour supprimer un produit Entrer 8 : ");
-		printf("\nPour afficher les statistique de vente en journée courante Entrer 9 : ");
+		printf("\nPour afficher les statistique de vente en journee courante Entrer 9 : ");
 		printf("\nPour quitter Entrer 0 : ");
 		printf("\nVotre choix : ");
 		scanf("%d",&choix);
@@ -57,8 +59,8 @@ void main() {
 				}
 				break;
 			case 3 :
-				printf("\nOrdre alphabétique croissant du nom 1 : ");
-                printf("\nOrdre décroissant du prix 2 : ");
+				printf("\nOrdre alphabetique croissant du nom 1 : ");
+                printf("\nOrdre decroissant du prix 2 : ");
                 printf("\nVotre choix : ");
                 scanf("%d",&Trie);
                 switch(Trie){
@@ -76,7 +78,7 @@ void main() {
 			case 4 :
 				printf("Entrer le code de produit que vous voulez acheter : ");
 				scanf("%s",code);
-                printf("Enter la quantité que vous voulez acheter : ");
+                printf("Enter la quantite que vous voulez acheter : ");
                 scanf("%d",&quant);
 				if(N==0){
 			    N = 1;
@@ -92,18 +94,19 @@ void main() {
 				break;
 			case 5 :
 	            printf("\nChercher par code 1 : ");
-                printf("\nChercher par quantité 2 : ");
+                printf("\nChercher par quantite 2 : ");
                 printf("\nVotre choix : ");
                 scanf("%d",&Search);
                 switch(Search){
                 	case 1 :
 						printf("Entrer le code de produit que vous voulez chercher : ");
-				        scanf("%d",&code);
-
+				        scanf("%s",code);
+                        searchcode(p,nbr,code);
                 		break;
                 	case 2 :
-						printf("Entrer le code de produit que vous voulez chercher : ");
-				        scanf("%d",&code);
+						printf("Entrer la quantite de produit que vous voulez chercher : ");
+				        scanf("%d",&quant);
+						searchquant(p,nbr,quant);
                 		break;
 					default :
 				        printf("\n\tChoix non valid !\n");
@@ -111,26 +114,52 @@ void main() {
 				}
 				break;
 			case 6 :
+			    printf("Les produits dont la quantite est inferieure a 3 : ");
+				etatstock(p,nbr);
 				break;
 			case 7 :
+			    printf("Entrer le code de produit que vous voulez allimenter : ");
+				scanf("%s",code);
+                printf("Enter la quantite : ");
+                scanf("%d",&quant);
+				p = alimenterstock(p,nbr,code,quant);
 				break;
 			case 8 :
+			    printf("Entrer le code de produit que vous voulez supprimer : ");
+				scanf("%s",code);
+				p = supprimer(p,nbr,code);
 				break;
 			case 9 :
-				printf("\nAfficher le total des prix des produits vendus en journée courante 1 : ");
-                printf("\nAfficher la moyenne des prix des produits vendus en journée courante 2 : ");
-                printf("\nAfficher le Max des prix des produits vendus en journée courante 3 : ");
-                printf("\nAfficher le Min des prix des produits vendus en journée courante 4 : ");
+				printf("\nAfficher le total des prix des produits vendus en journee courante 1 : ");
+                printf("\nAfficher la moyenne des prix des produits vendus en journee courante 2 : ");
+                printf("\nAfficher le Max des prix des produits vendus en journee courante 3 : ");
+                printf("\nAfficher le Min des prix des produits vendus en journee courante 4 : ");
                 printf("\nVotre choix : ");
                 scanf("%d",&Vente);
                 switch(Vente){
                 	case 1 :
+                    Total = total(A,N);
+                    if(Total==0 ){
+						printf("Aucune vente aujourd'hui");
+					}else printf("Le total de prix des vente est %d",Total);
                 		break;
                 	case 2 :
+					Total = total(A,N);
+					if(Total==0 ){
+						printf("Aucune vente aujourd'hui");
+					}else printf("La moyenne des prix des vente est %d",Moyenne);
                 		break;
                 	case 3 :
+					Total = total(A,N);
+					if(Total==0 ){
+						printf("Aucune vente aujourd'hui");
+					}else printf("Le Max des prix des produits vendus est %d",max(A,N));
                 		break;
                 	case 4 :
+					Total = total(A,N);
+					if(Total==0 ){
+						printf("Aucune vente aujourd'hui");
+					}else printf("Le Min des prix des produits vendus est %d",min(A,N));
                 		break;
 					default :
 				      printf("\n\tChoix non valid !\n");
